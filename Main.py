81fa055312733b5db1d987272d677043f0c0f2ef
maayan mordehai <maayanmordehai3@gmail.com>
2020-08-07 17:37:24 +0300

    changed to use __name__ instand of sending the func name

--- Main.py
+++ Main.py
@@ -23,16 +23,15 @@ def generate_worst_case_matrix(m):
     """
     return [[1 for i in range(0, m)] for i in range(0, m)]
 
-def test_cross_func(func_name, func, M, m):
+def test_cross_func(func, M, m):
     """
     This function tests a biggest Cross function execution time, and prints the findings.
     Parameters:
-        func_name - the name of the function we are testing
         func - the function
         M - the matrix we are using for the test
         m - the size of the matrix M
     """
-    print("testing {} on matrix size {}".format(func_name, m))
+    print("testing {} on matrix size {}".format(func.__name__, m))
     start_time = datetime.now()
     point, size = func(M, m)
     end_time = datetime.now()
@@ -40,7 +39,6 @@ def test_cross_func(func_name, func, M, m):
     print("time taken: {}".format(end_time - start_time))
 
 def main():
-    
     # Creating the test matrixs
     first_matrix_size = 15
     first_matrix =  generate_worst_case_matrix(first_matrix_size)
@@ -49,13 +47,13 @@ def main():
 
     # Testing bigCross1 O(m^3) vs bigCross2 O(m^2)
     print("=====================================================================")
-    test_cross_func("bigCross1", exercise1.bigCross1, first_matrix, first_matrix_size)
+    test_cross_func(exercise1.bigCross1, first_matrix, first_matrix_size)
     print("---------------------------------------------------------------------")
-    test_cross_func("bigCross2", exercise2.bigCross2, first_matrix, first_matrix_size)
+    test_cross_func(exercise2.bigCross2, first_matrix, first_matrix_size)
     print("=====================================================================")
-    test_cross_func("bigCross1", exercise1.bigCross1, second_matrix, second_matrix_size)
+    test_cross_func(exercise1.bigCross1, second_matrix, second_matrix_size)
     print("---------------------------------------------------------------------")
-    test_cross_func("bigCross2", exercise2.bigCross2, second_matrix, second_matrix_size)
+    test_cross_func(exercise2.bigCross2, second_matrix, second_matrix_size)
     print("=====================================================================")
 
 
