c3cc2a7e07c624b0b55f75d6c4fceb137e07bab5
Maayan Mordehai <maayanmordehai3@gmail.com>
2020-10-06 17:34:10 +0300

    added radix sort

--- general/sorts.py
+++ general/sorts.py
@@ -2,13 +2,13 @@ import random
 from heap import MaxHeap
 
 
-def heap_sort(lst, size):
+def heap_sort(lst):
     """
     This functions is sorting a list according to heap sort in O(nlog(n))
     :param lst: The list to sort
-    :param size: The size of the list
     :return: The sorted list
     """
+    size = len(lst)
     heap = MaxHeap(lst)
     sorted_lst = []
     for i in range(size):
@@ -16,12 +16,17 @@ def heap_sort(lst, size):
     return sorted_lst[::-1]
 
 
-def count_sort(lst, size, max_value=1000):
+def count_sort(lst, max_value=1000):
     """
     Count sort is for use when you know your value can't pass max_value.
-    Is good when your value options are not too many, if it is too many you can also have memory problem.
-    O(n)
+    k needs to be O(n), if k is O(n^2) then count sort is worst then comparing sorts of O(nlog(n))
+    O(k + n) (k is max_value)
+    count sort is unstable sort.
+    :param lst: The list to sort
+    :param max_value: The maximum value possible in the list
+    :return: The sorted list
     """
+    size = len(lst)
     counts = [0 for i in range(max_value+1)]
     # creating counts arr - O(size)
     for i in lst:
@@ -30,11 +35,8 @@ def count_sort(lst, size, max_value=1000):
         counts[i] += 1
 
     # setting each as the sum of its value and previse value - O(max_value)
-    pre = 0
-    for i in range(max_value+1):
-        counts[i] += pre
-        pre = counts[i]
-
+    for i in range(1, max_value+1):
+        counts[i] += counts[i - 1]
     # setting output list - O(size)
     output = [None for i in range(size)]
     for i in lst:
@@ -44,13 +46,62 @@ def count_sort(lst, size, max_value=1000):
     return output
 
 
+def radix_sort(lst, base=10):
+    """
+    Radix sort, sorting digit by digit. this is O(d*(n + k)) = O(n + k)
+    d - digit number, k - base, n - size of the list
+    Sorting first the lowest digits, and continuing until finished.
+    Radix sort need helper fot the sorting by digit. The helper must be stable sort algorithm.
+    Here using count sort with some changes for stability and by digit sort as helper.
+    :param lst:
+    """
+    maximum = max(lst)
+    count = 1
+    sorted_lst = lst
+    while int(maximum / count) > 0:
+        sorted_lst = _radix_helper(sorted_lst, count, base)
+        count *= base
+    return sorted_lst
+
+
+def _radix_helper(lst, count, base=10):
+    """
+    Using an adjusted count sort to sort each time the next digit
+    This is O(n + k) - k is the base
+    Adjust the count sort to be stable sort
+    :param lst: The list to sort
+    :param count: base ** number of time the function as run
+    :param base: The base for the counting - default 10
+    :return: The sorted list
+    """
+    size = len(lst)
+    counts = [0 for i in range(base)]
+    # creating counts arr - O(size)
+    for val in lst:
+        counts[int((val / count) % base)] += 1
+
+    # setting each as the sum of its value and previse value
+    for i in range(1, base):
+        counts[i] += counts[i - 1]
+
+    # setting output list - O(size)
+    output = [None for i in range(size)]
+    # passing on the list upside down for the sort to be stable
+    # stable sort is mandatory for radix algorithm
+    for val in lst[::-1]:
+        output[counts[int((val / count) % base)]-1] = val
+        counts[int((val / count) % base)] -= 1
+
+    return output
+
+
 def basic_testing_sort_func(func):
     print("============================================================")
     print("testing {} sorting function".format(func.__name__))
     for i in range(100):
-        a = [random.randint(0, 1000) for i in range(100)]
+        a = [random.randint(0, 1000) for i in range(10)]
         a_sorted = sorted(a)
-        h_sorted = func(a, len(a))
+        h_sorted = func(a)
         for j in range(len(a)):
             if a_sorted[j] != h_sorted[j]:
                 raise Exception(f"something wrong, sort did'nt pass on {a}")
@@ -60,6 +111,7 @@ def basic_testing_sort_func(func):
 def test():
     basic_testing_sort_func(heap_sort)
     basic_testing_sort_func(count_sort)
+    basic_testing_sort_func(radix_sort)
 
 
 if __name__ == "__main__":
